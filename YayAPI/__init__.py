0c5815813c431b360ee15e0ef474b9b301450345
konn-konn <hello@konn.ink>
2023-01-06 00:14:23 +0900

    __init__.pyファイルのバージョン修正

--- YayAPI/__init__.py
+++ YayAPI/__init__.py
@@ -1,3 +1,3 @@
 from .Yay import *
 
-__version__ = '1.0.0'
+__version__ = '1.0.6'
--- setup.py
+++ setup.py
@@ -4,23 +4,22 @@
 from setuptools import setup
 import YayAPI
 
-DESCRIPTION = "Unofficial api library for yay.space"
+DESCRIPTION = 'Unofficial api library for yay.space'
 NAME = 'YayAPI'
 AUTHOR = 'Konn'
 AUTHOR_EMAIL = 'hello@konn.ink'
 URL = 'https://github.com/konn-konn/YayAPI'
-
 DOWNLOAD_URL = 'https://github.com/konn-konn/YayAPI'
 VERSION = YayAPI.__version__
 
 INSTALL_REQUIRES = [
-    'requests~=2.28.1'
+    'requests>=2.25.1'
 ]
 PACKAGES = [
     'YayAPI'
 ]
 
-with open('README.md', 'r') as fp:
+with open('README.md', 'r', encoding='utf-8') as fp:
     readme = fp.read()
 long_description = readme
 
@@ -31,6 +30,7 @@ setup(name=NAME,
       maintainer_email=AUTHOR_EMAIL,
       description=DESCRIPTION,
       long_description=long_description,
+      long_description_content_type="text/markdown",
       url=URL,
       version=VERSION,
       download_url=DOWNLOAD_URL,
